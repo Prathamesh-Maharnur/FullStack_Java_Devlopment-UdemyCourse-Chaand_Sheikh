1db18ca56a256c2c75572001a24d2c670fdc4106
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-02-13 11:20:58 +0530

    2nd commit

---
+++ Course_Practice_files/S22_JSP_And_Servlets_Interacting_With_Database/S22L06-Adding_Database_Operations_In_Model/src/main/java/org/maharnur/prathamesh/config/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package org.maharnur.prathamesh.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+		public static Connection getConnection() {
+			
+			String url =
+				    "jdbc:mysql://localhost:3306/udemy_studyeasy?useSSL=false";
+			String userName = "root";
+			String userPassword = "cdac";
+			Connection con = null;
+			
+			try {
+				 Class.forName("com.mysql.cj.jdbc.Driver");
+				con = DriverManager.getConnection(url, userName, userPassword);
+			}catch(SQLException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return con;
+		}
+}
