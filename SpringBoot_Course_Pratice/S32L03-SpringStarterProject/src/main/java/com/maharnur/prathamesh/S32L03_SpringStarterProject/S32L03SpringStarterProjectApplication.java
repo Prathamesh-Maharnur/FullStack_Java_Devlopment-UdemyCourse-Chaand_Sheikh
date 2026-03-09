a5125837487f06594f039e2889d829d108b79aca
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-03-09 20:48:05 +0530

    added  S32L03-SpringStarterProject

---
+++ SpringBoot_Course_Pratice/S32L03-SpringStarterProject/src/main/java/com/maharnur/prathamesh/S32L03_SpringStarterProject/S32L03SpringStarterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.maharnur.prathamesh.S32L03_SpringStarterProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S32L03SpringStarterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S32L03SpringStarterProjectApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_Course_Pratice/S32L03-SpringStarterProject/src/test/java/com/maharnur/prathamesh/S32L03_SpringStarterProject/S32L03SpringStarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maharnur.prathamesh.S32L03_SpringStarterProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S32L03SpringStarterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
