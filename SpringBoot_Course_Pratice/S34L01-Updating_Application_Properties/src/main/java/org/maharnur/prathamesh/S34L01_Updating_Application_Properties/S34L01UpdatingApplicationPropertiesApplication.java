70901308ec80fa465647d58244b7fd96c2dfd3fb
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-03-10 17:41:46 +0530

    added S34L01-Updating_Application_Properties

---
+++ SpringBoot_Course_Pratice/S34L01-Updating_Application_Properties/src/main/java/org/maharnur/prathamesh/S34L01_Updating_Application_Properties/S34L01UpdatingApplicationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S34L01_Updating_Application_Properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S34L01UpdatingApplicationPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S34L01UpdatingApplicationPropertiesApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_Course_Pratice/S34L01-Updating_Application_Properties/src/test/java/org/maharnur/prathamesh/S34L01_Updating_Application_Properties/S34L01UpdatingApplicationPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S34L01_Updating_Application_Properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S34L01UpdatingApplicationPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
