1472c1f546d8bf8cfbe72ffe7e0ae16092610808
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-03-20 20:16:13 +0530

    added S35L05-Adding_Spring_Security_Into_Application

---
+++ SpringBoot_Course_Pratice/S35L05-Adding_Spring_Security_Into_Application/src/main/java/org/maharnur/prathamesh/S35L05_Adding_Spring_Security_Into_Application/S35L05AddingSpringSecurityIntoApplication.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S35L05_Adding_Spring_Security_Into_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S35L05AddingSpringSecurityIntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S35L05AddingSpringSecurityIntoApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_Course_Pratice/S35L05-Adding_Spring_Security_Into_Application/src/test/java/org/maharnur/prathamesh/S35L05_Adding_Spring_Security_Into_Application/S35L05AddingSpringSecurityIntoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S35L05_Adding_Spring_Security_Into_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S35L05AddingSpringSecurityIntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
