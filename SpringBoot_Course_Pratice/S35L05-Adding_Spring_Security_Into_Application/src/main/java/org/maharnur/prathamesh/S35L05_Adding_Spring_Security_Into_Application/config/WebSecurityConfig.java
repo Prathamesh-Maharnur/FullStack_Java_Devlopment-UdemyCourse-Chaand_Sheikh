beaa5b394f3fa13f363da1d309bf335a8ef1a1dc
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-04-02 12:31:58 +0530

    added full functionality in S35L05-Adding_Spring_Security_Into_Application

---
+++ SpringBoot_Course_Pratice/S35L05-Adding_Spring_Security_Into_Application/src/main/java/org/maharnur/prathamesh/S35L05_Adding_Spring_Security_Into_Application/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package org.maharnur.prathamesh.S35L05_Adding_Spring_Security_Into_Application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled=true, securedEnabled=true)
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+        .authorizeHttpRequests((auth) -> {
+            auth
+            .requestMatchers("/","/login","/register","/db-console/**",
+             "/css/**", "/fonts/**", "/images/**", "/js/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated();
+        });
+
+        http.csrf(csrf -> csrf.disable());
+        http
+            .headers(headers -> 
+                headers
+                .frameOptions(frameOptions -> frameOptions.disable()));
+        return http.build();
+    }
+}
