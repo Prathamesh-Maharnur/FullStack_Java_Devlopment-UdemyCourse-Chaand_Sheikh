b87cc69e4923b07f1f380259fbfd4b1d46d51f72
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-04-03 11:05:38 +0530

    S35L04 & S35L05 - removed version and scope in pom.xml for some manually added dependency

---
+++ SpringBoot_Course_Pratice/S35L06-SpringSecurityLoginRules_BCrypt/src/main/java/org/maharnur/prathamesh/S35L06_SpringSecurityLoginRules_BCrypt/S35L06SpringSecurityLoginRulesBCryptApplication.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S35L06_SpringSecurityLoginRules_BCrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S35L06SpringSecurityLoginRulesBCryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S35L06SpringSecurityLoginRulesBCryptApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_Course_Pratice/S35L06-SpringSecurityLoginRules_BCrypt/src/test/java/org/maharnur/prathamesh/S35L06_SpringSecurityLoginRules_BCrypt/S35L06SpringSecurityLoginRulesBCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S35L06_SpringSecurityLoginRules_BCrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S35L06SpringSecurityLoginRulesBCryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
