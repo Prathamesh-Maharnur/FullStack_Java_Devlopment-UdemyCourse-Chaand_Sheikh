d810229490f0d9737d3e4cf20aa7f83afa6c6da0
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-04-03 13:38:01 +0530

    removed useless methods in AccountService

--- SpringBoot_Course_Pratice/S35L05-Adding_Spring_Security_Into_Application/src/main/java/org/maharnur/prathamesh/S35L05_Adding_Spring_Security_Into_Application/services/AccountService.java
+++ SpringBoot_Course_Pratice/S35L05-Adding_Spring_Security_Into_Application/src/main/java/org/maharnur/prathamesh/S35L05_Adding_Spring_Security_Into_Application/services/AccountService.java
@@ -1,8 +1,5 @@
 package org.maharnur.prathamesh.S35L05_Adding_Spring_Security_Into_Application.services;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.maharnur.prathamesh.S35L05_Adding_Spring_Security_Into_Application.models.Account;
 import org.maharnur.prathamesh.S35L05_Adding_Spring_Security_Into_Application.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +10,6 @@ public class AccountService {
     
     @Autowired
     private AccountRepository accountRepository;
-
-    public Optional<Account> findById(Long id){
-        return accountRepository.findById(id);
-    }
-
-    public List<Account> getAll(){
-        return accountRepository.findAll();
-    }
-
-    public void delete(Account account){
-        accountRepository.delete(account);
-    }
 
     public Account save(Account account){
         return accountRepository.save(account);
