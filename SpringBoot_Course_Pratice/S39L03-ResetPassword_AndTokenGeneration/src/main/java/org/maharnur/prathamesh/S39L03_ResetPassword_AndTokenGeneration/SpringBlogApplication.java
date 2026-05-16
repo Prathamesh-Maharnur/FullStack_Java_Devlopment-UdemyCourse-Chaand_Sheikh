84e0a37d9380d8aef572684e162499686bbeaf05
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-05-16 11:57:58 +0530

    added S39L03-ResetPassword_AndTokenGeneration

---
+++ SpringBoot_Course_Pratice/S39L03-ResetPassword_AndTokenGeneration/src/main/java/org/maharnur/prathamesh/S39L03_ResetPassword_AndTokenGeneration/SpringBlogApplication.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S39L03_ResetPassword_AndTokenGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogApplication.class, args);
+	}
+
+}
