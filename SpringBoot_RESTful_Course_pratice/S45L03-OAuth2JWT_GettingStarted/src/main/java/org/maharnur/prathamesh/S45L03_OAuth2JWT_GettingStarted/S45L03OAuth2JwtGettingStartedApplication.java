02b863e3d286689274599314b40018bdcbc0b29a
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-05-25 23:14:35 +0530

    added template file for S45L03

---
+++ SpringBoot_RESTful_Course_pratice/S45L03-OAuth2JWT_GettingStarted/src/main/java/org/maharnur/prathamesh/S45L03_OAuth2JWT_GettingStarted/S45L03OAuth2JwtGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S45L03_OAuth2JWT_GettingStarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S45L03OAuth2JwtGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S45L03OAuth2JwtGettingStartedApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_RESTful_Course_pratice/S45L03-OAuth2JWT_GettingStarted/src/test/java/org/maharnur/prathamesh/S45L03_OAuth2JWT_GettingStarted/S45L03OAuth2JwtGettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maharnur.prathamesh.S45L03_OAuth2JWT_GettingStarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S45L03OAuth2JwtGettingStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
