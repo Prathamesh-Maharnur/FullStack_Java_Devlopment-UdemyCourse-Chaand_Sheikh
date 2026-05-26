31c77147e356eac1a6c0709e64117a9234a43c31
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-05-26 21:07:17 +0530

    S45L03: partial completion of Jwt encoder, decoder and Rsakey record

--- SpringBoot_RESTful_Course_pratice/S45L03-OAuth2JWT_GettingStarted/src/main/java/org/maharnur/prathamesh/S45L03_OAuth2JWT_GettingStarted/SpringRestDemoApplication.java
+++ SpringBoot_RESTful_Course_pratice/S45L03-OAuth2JWT_GettingStarted/src/main/java/org/maharnur/prathamesh/S45L03_OAuth2JWT_GettingStarted/SpringRestDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class S45L03OAuth2JwtGettingStartedApplication {
+public class SpringRestDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(S45L03OAuth2JwtGettingStartedApplication.class, args);
+		SpringApplication.run(SpringRestDemoApplication.class, args);
 	}
 
 }
