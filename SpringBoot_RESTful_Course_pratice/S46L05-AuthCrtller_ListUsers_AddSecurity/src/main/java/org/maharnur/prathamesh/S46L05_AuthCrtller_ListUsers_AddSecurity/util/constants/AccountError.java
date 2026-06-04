ca010046c813c470e42364bcc4386fda84952d55
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-06-04 21:35:18 +0530

    S46L05: added authorization(with help of authorities scope in jwt) for '/users', @ApiResponse docs 401 403

---
+++ SpringBoot_RESTful_Course_pratice/S46L05-AuthCrtller_ListUsers_AddSecurity/src/main/java/org/maharnur/prathamesh/S46L05_AuthCrtller_ListUsers_AddSecurity/util/constants/AccountError.java
@@ -0,0 +1,6 @@
+package org.maharnur.prathamesh.S46L05_AuthCrtller_ListUsers_AddSecurity.util.constants;
+
+public enum AccountError {
+    TOKEN_GENERATION_ERROR,
+    ADD_ACCOUNT_ERROR
+}
---
+++ SpringBoot_RESTful_Course_pratice/S46L05-AuthCrtller_ListUsers_AddSecurity/src/main/java/org/maharnur/prathamesh/S46L05_AuthCrtller_ListUsers_AddSecurity/util/constants/AccountSuccess.java
@@ -0,0 +1,5 @@
+package org.maharnur.prathamesh.S46L05_AuthCrtller_ListUsers_AddSecurity.util.constants;
+
+public enum AccountSuccess {
+    ACCOUNT_ADDED
+}
