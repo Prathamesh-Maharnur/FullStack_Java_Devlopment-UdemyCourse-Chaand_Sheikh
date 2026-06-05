eb66e878b0fe3f7f5fc918af4e385739e3ace9d8
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-06-05 21:05:43 +0530

    S46L06: added Authority enum and change the access to '/users' to ADMIN only

---
+++ SpringBoot_RESTful_Course_pratice/S46L06-AuthController_API_Authorization/src/main/java/org/maharnur/prathamesh/S46L06_AuthController_API_Authorization/SpringRestDemoApplication.java
@@ -0,0 +1,18 @@
+package org.maharnur.prathamesh.S46L06_AuthController_API_Authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@SpringBootApplication
+@SecurityScheme(name = "springboot-demo-api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+public class SpringRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDemoApplication.class, args);
+	}
+
+}
