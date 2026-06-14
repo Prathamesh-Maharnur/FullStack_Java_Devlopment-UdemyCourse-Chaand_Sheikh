5b4ea1c88b7b544dcb5b27a7a8c54e1f1c17f43b
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-06-14 21:21:26 +0530

    added PUT /users/update-authorities, created AuthoritiesDTO and defined findById() in AccountServ

---
+++ SpringBoot_RESTful_Course_pratice/S46L09-AuthController_UpdateAuthorities/src/main/java/org/maharnur/prathamesh/S46L09_AuthController_UpdateAuthorities/payload/auth/ProfileDTO.java
@@ -0,0 +1,17 @@
+package org.maharnur.prathamesh.S46L09_AuthController_UpdateAuthorities.payload.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProfileDTO {
+    
+    private long id; 
+
+    private String email;
+
+    private String authorities;
+}
---
+++ SpringBoot_RESTful_Course_pratice/S46L09-AuthController_UpdateAuthorities/src/main/java/org/maharnur/prathamesh/S46L09_AuthController_UpdateAuthorities/util/constants/AccountSuccess.java
@@ -0,0 +1,5 @@
+package org.maharnur.prathamesh.S46L09_AuthController_UpdateAuthorities.util.constants;
+
+public enum AccountSuccess {
+    ACCOUNT_ADDED
+}
