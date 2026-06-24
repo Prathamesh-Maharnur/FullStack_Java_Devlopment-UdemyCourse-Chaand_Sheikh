b0a090f0d5a49ab2be87a8e216354c0975f2e1f9
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-06-24 15:23:17 +0530

    added @SecurityScheme in SpringRestDemoApplication.java

--- SpringBoot_RESTful_Course_pratice/S46L12-Section_WrapUp/src/main/java/org/maharnur/prathamesh/S46L12_Section_WrapUp/SpringRestDemoApplication.java
+++ SpringBoot_RESTful_Course_pratice/S46L12-Section_WrapUp/src/main/java/org/maharnur/prathamesh/S46L12_Section_WrapUp/SpringRestDemoApplication.java
@@ -3,7 +3,12 @@ package org.maharnur.prathamesh.S46L12_Section_WrapUp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 @SpringBootApplication
+@SecurityScheme(name = "springboot-demo-api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class SpringRestDemoApplication {
 
 	public static void main(String[] args) {
