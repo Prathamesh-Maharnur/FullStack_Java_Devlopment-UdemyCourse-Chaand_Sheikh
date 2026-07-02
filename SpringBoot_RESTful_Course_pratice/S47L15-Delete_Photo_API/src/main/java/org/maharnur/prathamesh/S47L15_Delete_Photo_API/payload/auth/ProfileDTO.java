c66f01af94e81baaf1452ef497b2333d92fb8a7c
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-02 20:46:23 +0530

    S47L15: changed the type to List<PhotoViewDTO> of fileNamesWithSuccess for '/albums/{album_id}/upload-photo'as well return type, defined delete_photo_from_path() in AppUtils and delete() in photoService layer, defined DELETE '/albums/{album_id}/photos/{photos_id}/delete' in AlbumController

---
+++ SpringBoot_RESTful_Course_pratice/S47L15-Delete_Photo_API/src/main/java/org/maharnur/prathamesh/S47L15_Delete_Photo_API/payload/auth/ProfileDTO.java
@@ -0,0 +1,17 @@
+package org.maharnur.prathamesh.S47L15_Delete_Photo_API.payload.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProfileDTO {
+    
+    private long id; 
+
+    private String email;
+
+    private String authorities;
+}
---
+++ SpringBoot_RESTful_Course_pratice/S47L15-Delete_Photo_API/src/main/java/org/maharnur/prathamesh/S47L15_Delete_Photo_API/repository/PhotoRepository.java
@@ -0,0 +1,12 @@
+package org.maharnur.prathamesh.S47L15_Delete_Photo_API.repository;
+
+import java.util.List;
+
+import org.maharnur.prathamesh.S47L15_Delete_Photo_API.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Long>{
+    List<Photo> findByAlbum_id(long id);
+}
