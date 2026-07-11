05fb6ba7d17944e5eb4d8b998f51bd9460999409
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-11 23:42:10 +0530

    explored 'var' 'let' and 'const' keyword along with comment types

---
+++ JavaScript_Course_Pratice/S49L03-Comments_Variables_AndConstants/index.js
@@ -0,0 +1,69 @@
+var x = 10;
+
+console.log(x);
+
+var x = 7;
+
+console.log(x);
+
+{
+    var x = 30;
+    console.log(x);
+}
+
+console.log(x);
+
+// ***************************************
+
+console.log("***************************************");
+
+let y = 10;
+
+console.log(y);
+
+{
+    let y = 30;
+    console.log(y)
+}
+
+{
+    let x = 11;
+    console.log(x);
+    console.log(y);
+}
+
+console.log("x = "+x);
+
+{
+    let z = 33;
+}
+// console.log(z); // throw error
+
+let a = 12;
+console.log("a = "+a);
+a = "Prathamesh";
+console.log("a = "+a);
+
+// ***************************************
+
+console.log("***************************************");
+
+const PIE = 3.14;
+
+console.log(PIE);
+
+// PIE = 567; // throws error
+
+{
+    const PIE = 890;
+    console.log(PIE); 
+}
+
+console.log("***************************************");
+
+console.log("demo = "+demo);
+
+var demo = 123;
+
+console.log("demo = "+demo);
+
