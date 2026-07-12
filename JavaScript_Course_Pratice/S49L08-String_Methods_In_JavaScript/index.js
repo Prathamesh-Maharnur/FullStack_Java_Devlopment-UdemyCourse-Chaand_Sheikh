b32e0cc1d40c35bee479461dace2651bd0037749
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-13 00:01:42 +0530

    various String functions charAt() concat() endsWith() indexOf() lastIndex() replace(,) replaceAll() trim() trimEnd() toLowerCase() toUpperCase()

---
+++ JavaScript_Course_Pratice/S49L08-String_Methods_In_JavaScript/index.js
@@ -0,0 +1,38 @@
+let text = "Prathamesh";
+
+let msg = "             Hello World!       "
+console.log(text);
+
+console.log(text.charAt(6));
+
+text = text.concat("Maharnur");
+
+console.log(text);
+
+console.log(text.endsWith("h"));
+
+console.log(text.endsWith("r"));
+
+console.log(text.endsWith("t", 3));
+
+console.log(text.indexOf("m"));
+
+console.log(text.lastIndexOf("a"));
+
+console.log(text.indexOf("tha"));
+
+console.log(text.replace("nur", "athi"));
+
+console.log(text.replaceAll("a", "A"));
+
+console.log(msg.trim());
+
+console.log(msg.trimEnd());
+
+console.log(msg.trimStart());
+
+console.log(msg.toUpperCase());
+
+console.log(msg.toLowerCase());
+
+
