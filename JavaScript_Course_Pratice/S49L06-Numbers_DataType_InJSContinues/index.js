a2234e258d48d9a027d9f788ad960d9c6d8937d3
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-12 16:16:56 +0530

    S49L06: studied increment decrement operator, += -= *= /= string and int concatenation(with +) and NaN

---
+++ JavaScript_Course_Pratice/S49L06-Numbers_DataType_InJSContinues/index.js
@@ -0,0 +1,43 @@
+let result = 10;
+
+console.log(result);
+
+result = result++;
+
+console.log(result);
+
+result =  ++result;
+
+console.log(result);
+
+result += 3;
+
+console.log(result);
+
+result -= 2;
+
+console.log(result);
+
+result *= 2;
+
+console.log(result);
+
+result /= 4;
+
+console.log(result);
+
+result = 5 + "Prathamesh";
+console.log(result);
+
+result = 5 - "Prathamesh";
+console.log(result);
+
+result = 5 * "Prathamesh";
+console.log(result);
+
+result = 5 / "Prathamesh";
+console.log(result);
+
+let age = 23;
+
+console.log("My age is "+age);
