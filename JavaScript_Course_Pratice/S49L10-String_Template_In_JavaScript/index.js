2a06ab93883b1f28469ff4e2bb2ec37b343dbd6a
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-13 21:28:30 +0530

    S49L10: string template assigned to a variable

--- JavaScript_Course_Pratice/S49L10-String_Template_In_JavaScript/index.js
+++ JavaScript_Course_Pratice/S49L10-String_Template_In_JavaScript/index.js
@@ -4,3 +4,7 @@ let name2 = "Shyam";
 console.log("Hello! "+ name1.toUpperCase()+ ", Hello! " + name2);
 
 console.log(`Hello! ${name1.toUpperCase()}, Hello! ${name2}`);
+
+let text= `Hello! ${name1.toUpperCase()}, Hello! ${name2}`;
+
+console.log(text);
