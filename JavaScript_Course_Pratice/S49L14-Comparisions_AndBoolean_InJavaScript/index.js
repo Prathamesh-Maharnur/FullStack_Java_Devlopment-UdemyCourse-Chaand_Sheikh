82c8e9703a15fe3bfab19cf07ea50ea4c2f8f447
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-16 18:13:03 +0530

    S49L14: explored ==, !=, >, <, >=, <=, ! operators

---
+++ JavaScript_Course_Pratice/S49L14-Comparisions_AndBoolean_InJavaScript/index.js
@@ -0,0 +1,15 @@
+let x = 10;
+
+console.log(x == 10);
+
+console.log(x != 10);
+
+console.log(x > 10);
+
+console.log(x < 10);
+
+console.log(x >= 10);
+
+console.log(x <= 10);
+
+console.log(!(x == 10));
