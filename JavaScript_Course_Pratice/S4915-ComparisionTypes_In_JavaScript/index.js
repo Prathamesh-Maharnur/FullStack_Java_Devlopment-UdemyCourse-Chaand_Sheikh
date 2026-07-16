5d2abbf9c1fc56f56a5c1ca1f5f954bebb12c428
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-17 00:06:16 +0530

    S49L15: explored loose comparision(==) and  strict comparision(=== and !==)

---
+++ JavaScript_Course_Pratice/S4915-ComparisionTypes_In_JavaScript/index.js
@@ -0,0 +1,20 @@
+let x = 10;
+
+console.log(x == '10');
+console.log(x == 10);
+
+let result = (x == 11);
+
+console.log(result);
+result = (x == '10');
+console.log(result);
+
+console.log("----------------------");
+
+let y = 11;
+
+console.log(y === 11);
+console.log(y === '11');
+console.log(y !== 11);
+console.log(y !== '11');
+console.log(y !== 12);
