2602f33ad801df944effd7fa3d4259caba2e2962
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-21 15:06:28 +0530

    S51L02: while loop in JS

---
+++ JavaScript_Course_Pratice/S51L02-WhileLoop_In_JavaScript/index.js
@@ -0,0 +1,13 @@
+let i =0;
+while(i<5){
+    console.log(i);
+    i++;
+}
+
+const names = ['Prathamesh', 'Mayur', 'Vinayak', 'Jitendra', 'Vivek'];
+
+i=0;            
+while(i < names.length){
+    console.log(names[i]);
+    i++;
+}
