aeb72900c4b8108d4681b34ba050edd675258b18
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-22 23:11:16 +0530

    S52L01: explored switch case in JS

---
+++ JavaScript_Course_Pratice/S52L01-SwitchCase_In_JavaScript/index.js
@@ -0,0 +1,21 @@
+const grade = 'B';
+
+switch(grade){
+    case 'A':
+        console.log("Excellent");
+        break;
+    case 'B':
+        console.log("Good");
+        break;
+    case 'C':
+        console.log("Average");
+        break;
+    case "D":
+        console.log("Bad");
+        break;
+    case 'E':
+        console.log("Fail");
+        break;
+    default:
+        console.log("Invalid grade");   
+}
