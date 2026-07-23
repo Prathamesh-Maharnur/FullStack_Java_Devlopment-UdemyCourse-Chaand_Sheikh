8e8eca32deb030118fb1d7c0eaf7261779e1c751
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-23 21:34:09 +0530

    explored function parameters, arguments and default value

---
+++ JavaScript_Course_Pratice/S53L02-FunctionParameters_And_Arguments/index.js
@@ -0,0 +1,14 @@
+const message = function(name="Shyam", topic="Java", experience="fun"){
+    console.log(`This is me, ${name}`);
+    console.log(`I am learning ${topic}`);
+    console.log(`And this is ${experience}`)
+};
+
+message();
+console.log("--------------------------------");
+message("Prathamesh");
+console.log("--------------------------------");
+message("Prathamesh", "SpringBoot");
+console.log("--------------------------------");
+message("Prathamesh", "SpringBoot", "Great");
+
