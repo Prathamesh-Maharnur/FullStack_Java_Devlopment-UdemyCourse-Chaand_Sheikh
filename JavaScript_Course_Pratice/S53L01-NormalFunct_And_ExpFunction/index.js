ffb97623403002089d764b3cbc661246b1771cfe
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-23 18:03:32 +0530

    S53L01: explored function, function hoisting, function expression

---
+++ JavaScript_Course_Pratice/S53L01-NormalFunct_And_ExpFunction/index.js
@@ -0,0 +1,26 @@
+message();
+message();
+
+function message(){
+    console.log("Hello I am Prathamesh");
+    console.log("I am learning JS");
+    console.log("It's fun to learn JS");
+}
+
+console.log("------------------------------");
+
+message();
+message();
+
+console.log("------------------------------");
+
+// demo(); // throws error
+
+const demo = function(){
+    console.log("Hello I am Prathamesh");
+    console.log("I am learning JS");
+    console.log("It's fun to learn JS")
+}
+
+demo();
+demo();
