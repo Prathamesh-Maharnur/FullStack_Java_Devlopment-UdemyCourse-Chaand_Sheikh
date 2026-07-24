49144c578ada095f96583c10a128ffd4ef64443f
Prathamesh-Maharnur <prathameshmaharnur9967@gmail.com>
2026-07-24 23:20:35 +0530

    S53L03: explored return keyword

---
+++ JavaScript_Course_Pratice/S53L03-ReturnValues_From_Functions/index.js
@@ -0,0 +1,13 @@
+let add1 = function(a,b){
+    return (a+b);
+}
+
+let add2 = function(a,b){
+    console.log(`The result is: ${a+b}`);
+}
+
+let result = add1(10, 5);
+
+let result = add2(10, 5);
+console.log(`The result is: ${result}`);
+
